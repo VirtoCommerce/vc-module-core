4cc91ea2ebd9a225c8fa1b2893b4b83333606d2f
artem-dudarev <ad@virtoway.com>
2015-12-11 10:55:47 +0200

    Moved products searching to the search module.

---
+++ VirtoCommerce.Domain/Catalog/Model/Search/Facet.cs
@@ -0,0 +1,31 @@
+﻿namespace VirtoCommerce.Domain.Catalog.Model
+{
+    public class Facet
+    {
+        /// <summary>
+        /// Gets or sets the value of facet type
+        /// </summary>
+        /// <value>
+        /// "Attribute", "PriceRange", "Range" or "Category"
+        /// </value>
+        public string FacetType { get; set; }
+
+        /// <summary>
+        /// Gets or sets the value of facet field
+        /// </summary>
+        public string Field { get; set; }
+
+        /// <summary>
+        /// Gets or sets the value of facet label
+        /// </summary>
+        public string Label { get; set; }
+
+        /// <summary>
+        /// Gets or sets the collection of facet values
+        /// </summary>
+        /// <value>
+        /// Array of FacetValue objects
+        /// </value>
+        public FacetValue[] Values { get; set; }
+    }
+}
---
+++ VirtoCommerce.Domain/Catalog/Model/Search/FacetValue.cs
@@ -0,0 +1,25 @@
+﻿namespace VirtoCommerce.Domain.Catalog.Model
+{
+    public class FacetValue
+    {
+        /// <summary>
+        /// Gets or sets the facet value count
+        /// </summary>
+        public int Count { get; set; }
+
+        /// <summary>
+        /// Gets or sets the flag for facet value is applied
+        /// </summary>
+        public bool IsApplied { get; set; }
+
+        /// <summary>
+        /// Gets or sets the facet value label
+        /// </summary>
+        public string Label { get; set; }
+
+        /// <summary>
+        /// Gets or sets the facet value
+        /// </summary>
+        public object Value { get; set; }
+    }
+}
