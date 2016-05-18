a8e12585da42f33df95ec964832538e323b870d8
Tatarincev <et@virtoway.com>
2016-05-18 11:44:48 +0200

    Fix getting actual dictionary property value from meta-information

--- VirtoCommerce.Domain/Catalog/Model/PropertyValue.cs
+++ VirtoCommerce.Domain/Catalog/Model/PropertyValue.cs
@@ -6,21 +6,46 @@ using VirtoCommerce.Domain.Commerce.Model;
 using VirtoCommerce.Platform.Core.Common;
 namespace VirtoCommerce.Domain.Catalog.Model
 {
-	public class PropertyValue : AuditableEntity, ILanguageSupport, IInheritable, ICloneable
+    public class PropertyValue : AuditableEntity, ILanguageSupport, IInheritable, ICloneable
     {
-		public string PropertyId { get; set; }
-		public string PropertyName { get; set; }
-		public Property Property { get; set; }
-		public string Alias { get; set; }
-		public string ValueId { get; set; }
-		public object Value { get; set; }
-		public PropertyValueType ValueType { get; set; }
-		public string LanguageCode { get; set; }
+        public string PropertyId { get; set; }
+        public string PropertyName { get; set; }
+        public Property Property { get; set; }
+        public string Alias { get; set; }
+        public string ValueId { get; set; }
 
-		public override string ToString()
-		{
-			return (PropertyName ?? "unknown") + ":" + (Value ?? "undefined");
-		}
+        private object _value;
+        public object Value
+        {
+            get
+            {
+                var retVal = _value;
+                /// Return actual dictionary property value from property meta-information instead stored
+                if (Property != null && Property.Dictionary && Property.DictionaryValues != null && ValueId != null)
+                {
+                    var dictValue = Property.DictionaryValues.FirstOrDefault(x => x.Id == ValueId);
+                    if (dictValue != null)
+                    {
+                        retVal = dictValue.Value;
+                    }
+
+                }
+                return retVal;
+            }
+            set
+            {
+                _value = value;
+            }
+        }
+
+        public PropertyValueType ValueType { get; set; }
+        public string LanguageCode { get; set; }
+
+        public override string ToString()
+        {
+            return (PropertyName ?? "unknown") + ":" + (Value ?? "undefined");
+        }
+
 
         /// <summary>
         /// Returns for current value all dictionary values in all defined languages 
@@ -29,7 +54,7 @@ namespace VirtoCommerce.Domain.Catalog.Model
         public IEnumerable<PropertyValue> TryGetAllLocalizedDictValues()
         {
             var retVal = new List<PropertyValue>();
-           
+
             if (Property != null && Property.Dictionary && Property.Multilanguage && Property.DictionaryValues != null)
             {
                 foreach (var dictValue in Property.DictionaryValues.Where(x => x.Alias == Alias))
@@ -67,7 +92,7 @@ namespace VirtoCommerce.Domain.Catalog.Model
             retVal.Value = Value;
             retVal.ValueType = ValueType;
             retVal.IsInherited = IsInherited;
-           
+
             return retVal;
         }
         #endregion
