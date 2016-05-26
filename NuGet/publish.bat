6eded164408afb001c9199facfd580a483c681d3
artem-dudarev <ad@virtoway.com>
2016-05-26 10:42:28 +0200

    Updated version in Nuget\publish.bat

--- NuGet/publish.bat
+++ NuGet/publish.bat
@@ -1,3 +1,4 @@
-set V=2.10.1
+set V=2.10.2
 nuget push VirtoCommerce.Domain.%V%.nupkg -Source nuget.org -ApiKey %1
 nuget push VirtoCommerce.CoreModule.Data.%V%.nupkg -Source nuget.org -ApiKey %1
+pause
