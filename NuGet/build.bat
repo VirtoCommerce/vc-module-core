b3f5d1523e000749eedcd49141457f6b86e07916
artem-dudarev <ad@virtoway.com>
2016-06-01 15:44:53 +0200

    Added VirtoCommerce.CoreModule.Client

--- NuGet/build.bat
+++ NuGet/build.bat
@@ -8,6 +8,7 @@ IF NOT DEFINED MSBUILD_PATH SET MSBUILD_PATH=%WINDIR%\Microsoft.NET\Framework\v4
 
 "%MSBUILD_PATH%" "%SOURCE_DIR%\VirtoCommerce.CoreModule.sln" /nologo /verbosity:m /t:Build /p:Configuration=Release;Platform="Any CPU"
 
+nuget pack "%SOURCE_DIR%\VirtoCommerce.CoreModule.Client\VirtoCommerce.CoreModule.Client.csproj" -IncludeReferencedProjects -Symbols -Properties Configuration=Release -o "%TARGET_DIR%"
 nuget pack "%SOURCE_DIR%\VirtoCommerce.Domain\VirtoCommerce.Domain.csproj" -IncludeReferencedProjects -Symbols -Properties Configuration=Release -o "%TARGET_DIR%"
 nuget pack "%SOURCE_DIR%\VirtoCommerce.CoreModule.Data\VirtoCommerce.CoreModule.Data.csproj" -IncludeReferencedProjects -Symbols -Properties Configuration=Release -o "%TARGET_DIR%"
 
