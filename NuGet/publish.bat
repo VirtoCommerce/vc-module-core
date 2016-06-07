30260c6a89c148f2a02a4ed8d36fcc47d47fc776
Sasha <Woland2k@users.noreply.github.com>
2016-06-06 17:49:23 -0700

    Update publish.bat

--- NuGet/publish.bat
+++ NuGet/publish.bat
@@ -2,4 +2,3 @@ set V=2.10.5
 nuget push VirtoCommerce.CoreModule.Client.%V%.nupkg -Source nuget.org -ApiKey %1
 nuget push VirtoCommerce.Domain.%V%.nupkg -Source nuget.org -ApiKey %1
 nuget push VirtoCommerce.CoreModule.Data.%V%.nupkg -Source nuget.org -ApiKey %1
-pause
