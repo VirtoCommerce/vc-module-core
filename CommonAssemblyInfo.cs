89dfd9956532a09f1486cb18a923986fbfbc7deb
Virto Jenkins <ci@virtocommerce.com>
2016-06-15 01:05:17 +0000

    Updated version number jenkins-Virto Commerce 2.x Modules Build-vc-module-core-master-97

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Virto Commerce Core Module")]
 [assembly: AssemblyCopyright("Copyright © VirtoCommerce 2011-2016")]
 
-[assembly: AssemblyFileVersion("2.11.1.0")]
+[assembly: AssemblyFileVersion("2.11.2.0")]
 [assembly: AssemblyVersion("2.11.0.0")]
 
 #if DEBUG
@@ -26,4 +26,5 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
+
 
