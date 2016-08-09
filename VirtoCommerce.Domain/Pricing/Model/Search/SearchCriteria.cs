71d4865b892d91d96a6e59f05953709c52ffc97a
Tatarincev <et@virtoway.com>
2016-08-09 12:52:47 +0200

    Add extra fields to pricing SearchCriteria

--- VirtoCommerce.Domain/Pricing/Model/Search/SearchCriteria.cs
+++ VirtoCommerce.Domain/Pricing/Model/Search/SearchCriteria.cs
@@ -11,8 +11,13 @@ namespace VirtoCommerce.Domain.Pricing.Model.Search
     {
         public SearchCriteria()
         {
-            Count = 20;
+            Take = 20;
         }
+
+        //Apply paginate limits to product count instead prices record
+        public bool GroupByProducts { get; set; }
+
+        public string Keyword { get; set; }
 
         /// <summary>
         /// Sorting expression property1:asc;property2:desc
@@ -46,7 +51,26 @@ namespace VirtoCommerce.Domain.Pricing.Model.Search
             }
         }
 
-        public int Start { get; set; }
-        public int Count { get; set; }
+        public string ProductId { get; set; }
+
+        private string[] _productIds;
+        public string[] ProductIds
+        {
+            get
+            {
+                if (_productIds == null && !string.IsNullOrEmpty(ProductId))
+                {
+                    _productIds = new[] { ProductId };
+                }
+                return _productIds;
+            }
+            set
+            {
+                _productIds = value;
+            }
+        }
+
+        public int Skip { get; set; }
+        public int Take { get; set; }
     }
 }
