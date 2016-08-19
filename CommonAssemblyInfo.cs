4141a3ade3d4152c2087d8485643c8b9c2ee894d
Tatarincev <et@virtoway.com>
2016-08-19 14:06:23 +0200

    Update assembly version

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -15,8 +15,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Virto Commerce Core Module")]
 [assembly: AssemblyCopyright("Copyright © VirtoCommerce 2011-2016")]
 
-[assembly: AssemblyFileVersion("2.16.0.0")]
-[assembly: AssemblyVersion("2.16.0.0")]
+[assembly: AssemblyFileVersion("2.16.1.0")]
+[assembly: AssemblyVersion("2.16.1.0")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
