a87588a6ff812ef29d6ba9ea09b19b240c19c3cc
Tatarincev <et@virtoway.com>
2017-05-22 08:32:35 +0200

    Add new IAssociationService

---
+++ VirtoCommerce.Domain/Catalog/Services/IAssociationService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtoCommerce.Domain.Catalog.Model;
+
+namespace VirtoCommerce.Domain.Catalog.Services
+{
+    public interface IAssociationService
+    {
+        ProductAssociation GetByIds(string[] ids);
+        void SaveChanges(ProductAssociation[] associations);
+        void Delete(string[] ids);
+    }
+}
