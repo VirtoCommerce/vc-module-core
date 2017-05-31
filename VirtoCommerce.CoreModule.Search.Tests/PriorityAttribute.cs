d4d45703c43e76affa11ae082a74be75bc82381e
artem-dudarev <ad@virtoway.com>
2017-05-31 10:50:27 +0200

    Added search provider tests

---
+++ VirtoCommerce.CoreModule.Search.Tests/PriorityAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace VirtoCommerce.CoreModule.Search.Tests
+{
+    public class PriorityAttribute : Attribute
+    {
+        public PriorityAttribute(int priority)
+        {
+            Priority = priority;
+        }
+
+        public int Priority { get; }
+    }
+}
