d7a1ab990b42eaca00f8a5ed8d30f791e9942e3c
ksa <ksa@virtoway.com>
2017-07-06 14:23:55 +0200

    added IPropertyValueValidator abstraction

---
+++ VirtoCommerce.Domain/Catalog/Services/IPropertyValueValidator.cs
@@ -0,0 +1,9 @@
+﻿using VirtoCommerce.Domain.Catalog.Model;
+
+namespace VirtoCommerce.Domain.Catalog.Services
+{
+    public interface IPropertyValueValidator
+    {
+        string[] Validate(PropertyValidationRule validationRule, PropertyValue value);
+    }
+}
