41d66c7f0ae4e729efeeeeb83f85ad52197bd806
artem-dudarev <ad@virtoway.com>
2017-07-07 10:08:25 +0200

    Prevented concurrent indexation, added push notifications to recurring jobs, manual indexation updates indexation date for recurring job.

--- VirtoCommerce.CoreModule.Web/BackgroundJobs/IndexingJobs.cs
+++ VirtoCommerce.CoreModule.Web/BackgroundJobs/IndexingJobs.cs
@@ -1,10 +1,17 @@
-﻿using System;
+﻿// This indexing jobs implementation allows only one job to perform indexing.
+// If some job is started successfully, all other jobs will terminate with "Indexation is already in progress" error until the first job is finished.
+// The synchronization is done by checking the static field _cancellationTokenSource. If it is not null, then some job is already running.
+// This scenario requires the platform and hangfire server to run in the same application.
+
+// TODO: Implement indexation for multiple servers. Distribute different document partions between servers for parallel indexation.
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Common.Logging;
 using Hangfire;
+using VirtoCommerce.CoreModule.Web.Model.PushNotifcations;
 using VirtoCommerce.Domain.Search;
 using VirtoCommerce.Platform.Core.Common;
 using VirtoCommerce.Platform.Core.Settings;
@@ -13,39 +20,152 @@ namespace VirtoCommerce.CoreModule.Web.BackgroundJobs
 {
     public class IndexingJobs
     {
+        private static readonly object _lockObject = new object();
+        private static CancellationTokenSource _cancellationTokenSource;
+
         private readonly IndexDocumentConfiguration[] _documentsConfigs;
         private readonly IIndexingManager _indexingManager;
         private readonly ISettingsManager _settingsManager;
-        private readonly ILog _logging;
+        private readonly IndexProgressHandler _progressHandler;
 
-        public IndexingJobs(IndexDocumentConfiguration[] documentsConfigs, IIndexingManager indexingManager, ISettingsManager settingsManager, ILog logging)
+        public IndexingJobs(IndexDocumentConfiguration[] documentsConfigs, IIndexingManager indexingManager, ISettingsManager settingsManager, IndexProgressHandler progressHandler)
         {
             _documentsConfigs = documentsConfigs;
             _indexingManager = indexingManager;
             _settingsManager = settingsManager;
-            _logging = logging;
+            _progressHandler = progressHandler;
         }
 
-        [DisableConcurrentExecution(60 * 60 * 24)]
-        public void ProcessChanges(string documentType)
+        // Enqueue a background job with single notification object for all given options
+        public static IndexProgressPushNotification Enqueue(string currentUserName, IndexingOptions[] options)
+        {
+            var notification = IndexProgressHandler.CreateNotification(currentUserName, null);
+            BackgroundJob.Enqueue<IndexingJobs>(j => j.IndexAllDocumentsJob(currentUserName, notification.Id, options));
+            return notification;
+        }
+
+        // Cancel current indexation if there is one
+        public static void CancelIndexation()
+        {
+            lock (_lockObject)
+            {
+                _cancellationTokenSource?.Cancel();
+            }
+        }
+
+        // One-time job for manual indexation
+        public async Task IndexAllDocumentsJob(string userName, string notificationId, IndexingOptions[] options)
+        {
+            // Use single notification for all options (document types)
+            await IndexAsync(userName, notificationId, false, options, IndexAllDocumentsAsync);
+        }
+
+        // Recurring job for automatic changes indexation.
+        // It should push separate notification for each document type if any changes were indexed for this type
+        public async Task IndexChangesJob(string documentType)
         {
             var allOptions = GetAllIndexingOptions(documentType);
 
+            // Create different notification for each option (document type)
             foreach (var options in allOptions)
             {
-                var now = DateTime.UtcNow;
-                options.EndDate = options.StartDate != null ? (DateTime?)now : null;
-
-                Index(options);
-
-                SetLastIndexationDate(options.DocumentType, options.StartDate, now);
+                await IndexAsync(null, null, true, new[] { options }, IndexChangesAsync);
             }
         }
 
 
-        private void Index(IndexingOptions options)
+        private async Task IndexAsync(string currentUserName, string notificationId, bool suppressInsignificantNotifications, IEnumerable<IndexingOptions> allOptions, Func<IndexingOptions, CancellationToken, Task> indexationFunc)
         {
-            Task.Run(() => _indexingManager.IndexAsync(options, p => _logging.Trace(p.Description), CancellationToken.None)).GetAwaiter().GetResult();
+            // Reset progress handler to initial state
+            _progressHandler.Start(currentUserName, notificationId, suppressInsignificantNotifications);
+
+            // Capture the syncronization object (cancelation token source) if it is not captured already.
+            var cancellationTokenSource = StartIndexation();
+
+            if (cancellationTokenSource == null)
+            {
+                // Report error and exit
+                _progressHandler.AlreadyInProgress();
+            }
+            else
+            {
+                // Begin indexation
+                try
+                {
+                    foreach (var options in allOptions)
+                    {
+                        await indexationFunc(options, cancellationTokenSource.Token);
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    _progressHandler.Cancel();
+                }
+                catch (Exception ex)
+                {
+                    _progressHandler.Exception(ex);
+                }
+                finally
+                {
+                    // Report indexation summary
+                    _progressHandler.Finish();
+
+                    // Release the syncronization object
+                    FinishIndexation();
+                }
+            }
+        }
+
+        private async Task IndexAllDocumentsAsync(IndexingOptions options, CancellationToken cancellationToken)
+        {
+            var oldIndexationDate = GetLastIndexationDate(options.DocumentType);
+            var newIndexationDate = DateTime.UtcNow;
+
+            await _indexingManager.IndexAsync(options, _progressHandler.Progress, cancellationToken);
+
+            // Save indexation date to prevent changes from being indexed again
+            SetLastIndexationDate(options.DocumentType, oldIndexationDate, newIndexationDate);
+        }
+
+        private async Task IndexChangesAsync(IndexingOptions options, CancellationToken cancellationToken)
+        {
+            var oldIndexationDate = options.StartDate;
+            var newIndexationDate = DateTime.UtcNow;
+
+            options.EndDate = oldIndexationDate == null ? null : (DateTime?)newIndexationDate;
+
+            await _indexingManager.IndexAsync(options, _progressHandler.Progress, cancellationToken);
+
+            // Save indexation date. It will be used as a start date for the next indexation
+            SetLastIndexationDate(options.DocumentType, oldIndexationDate, newIndexationDate);
+        }
+
+        private static CancellationTokenSource StartIndexation()
+        {
+            CancellationTokenSource result = null;
+
+            lock (_lockObject)
+            {
+                if (_cancellationTokenSource == null)
+                {
+                    _cancellationTokenSource = new CancellationTokenSource();
+                    result = _cancellationTokenSource;
+                }
+            }
+
+            return result;
+        }
+
+        private static void FinishIndexation()
+        {
+            lock (_lockObject)
+            {
+                if (_cancellationTokenSource != null)
+                {
+                    _cancellationTokenSource.Cancel();
+                    _cancellationTokenSource = null;
+                }
+            }
         }
 
         private IList<IndexingOptions> GetAllIndexingOptions(string documentType)
--- VirtoCommerce.CoreModule.Web/Controllers/Api/SearchIndexationController.cs
+++ VirtoCommerce.CoreModule.Web/Controllers/Api/SearchIndexationController.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Description;
-using Hangfire;
+using VirtoCommerce.CoreModule.Web.BackgroundJobs;
 using VirtoCommerce.CoreModule.Web.Model.PushNotifcations;
 using VirtoCommerce.CoreModule.Web.Security;
 using VirtoCommerce.Domain.Search;
-using VirtoCommerce.Platform.Core.Common;
 using VirtoCommerce.Platform.Core.PushNotifications;
 using VirtoCommerce.Platform.Core.Security;
 using VirtoCommerce.Platform.Core.Web.Security;
@@ -20,9 +16,6 @@ namespace VirtoCommerce.CoreModule.Web.Controllers.Api
     [ApiExplorerSettings(IgnoreApi = true)]
     public class SearchIndexationController : ApiController
     {
-        private static readonly object _lockObject = new object();
-        private static readonly Dictionary<string, CancellationTokenSource> _runningProcesses = new Dictionary<string, CancellationTokenSource>();
-
         private readonly IndexDocumentConfiguration[] _documentConfigs;
         private readonly ISearchProvider _searchProvider;
         private readonly IIndexingManager _indexingManager;
@@ -79,16 +72,9 @@ namespace VirtoCommerce.CoreModule.Web.Controllers.Api
         [CheckPermission(Permission = CommercePredefinedPermissions.IndexRebuild)]
         public IHttpActionResult IndexDocuments([FromBody] IndexingOptions[] options)
         {
-            var notification = new IndexProgressPushNotification(_userNameResolver.GetCurrentUserName())
-            {
-                Title = "Indexation process",
-                Description = "Starting indexation..."
-            };
-
+            var currentUserName = _userNameResolver.GetCurrentUserName();
+            var notification = IndexingJobs.Enqueue(currentUserName, options);
             _pushNotifier.Upsert(notification);
-
-            BackgroundJob.Enqueue(() => BackgroundIndex(options, notification));
-
             return Ok(notification);
         }
 
@@ -97,78 +83,8 @@ namespace VirtoCommerce.CoreModule.Web.Controllers.Api
         [Route("tasks/{taskId}/cancel")]
         public IHttpActionResult CancelIndexationProcess(string taskId)
         {
-            lock (_lockObject)
-            {
-                if (_runningProcesses.ContainsKey(taskId))
-                {
-                    _runningProcesses[taskId].Cancel();
-                }
-            }
-
+            IndexingJobs.CancelIndexation();
             return Ok();
-        }
-
-        // Only public methods can be invoked in the background. (Hangfire)
-        public void BackgroundIndex(IndexingOptions[] options, IndexProgressPushNotification notification)
-        {
-            var totalCountMap = new Dictionary<string, long>();
-            var processedCountMap = new Dictionary<string, long>();
-            Action<IndexingProgress> progressCallback = x =>
-            {
-                notification.DocumentType = x.DocumentType;
-                notification.Description = x.Description;
-                if (!x.Errors.IsNullOrEmpty())
-                {
-                    notification.Errors.AddRange(x.Errors);
-                    notification.ErrorCount = notification.Errors.Count();
-                }              
-                notification.TotalCount = x.TotalCount ?? 0;
-                notification.ProcessedCount = x.ProcessedCount ?? 0;
-                totalCountMap[x.DocumentType] = x.TotalCount ?? 0;
-                processedCountMap[x.DocumentType] = x.ProcessedCount ?? 0;
-                _pushNotifier.Upsert(notification);
-            };
-
-            var cancellationTokenSource = new CancellationTokenSource();
-
-            lock (_lockObject)
-            {
-                _runningProcesses[notification.Id] = cancellationTokenSource;
-            }
-
-            try
-            {
-                var cancellationToken = cancellationTokenSource.Token;
-                foreach (var option in options)
-                {
-                    _indexingManager.IndexAsync(option, progressCallback, cancellationToken).Wait(cancellationToken);
-                }
-            }
-            catch (OperationCanceledException)
-            {
-            }
-            catch (Exception ex)
-            {
-                notification.Description = "Index error";
-                notification.ErrorCount++;
-                notification.Errors.Add(ex.ToString());
-            }
-            finally
-            {
-
-                notification.Finished = DateTime.UtcNow;
-                notification.TotalCount = totalCountMap.Values.Sum();
-                notification.ProcessedCount = processedCountMap.Values.Sum();
-                if (cancellationTokenSource.IsCancellationRequested)
-                {
-                    notification.Description = "Indexation canceled";
-                }
-                else
-                {
-                    notification.Description = "Indexation " + (notification.Errors?.Any() == true ? " with errors" : " successfully");
-                }
-                _pushNotifier.Upsert(notification);
-            }
         }
     }
 }
