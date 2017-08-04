d466c504e843c210428ac39039ec071a33449af0
artem-dudarev <ad@virtoway.com>
2017-08-04 15:47:20 +0200

    Use ConnectionStringHelper

--- VirtoCommerce.CoreModule.Web/Module.cs
+++ VirtoCommerce.CoreModule.Web/Module.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Configuration;
 using System.Linq;
 using System.Web.Http;
 using Hangfire;
@@ -22,6 +21,7 @@ using VirtoCommerce.Domain.Payment.Services;
 using VirtoCommerce.Domain.Search;
 using VirtoCommerce.Domain.Shipping.Services;
 using VirtoCommerce.Domain.Tax.Services;
+using VirtoCommerce.Platform.Core.Common;
 using VirtoCommerce.Platform.Core.ExportImport;
 using VirtoCommerce.Platform.Core.Modularity;
 using VirtoCommerce.Platform.Core.Settings;
@@ -32,7 +32,7 @@ namespace VirtoCommerce.CoreModule.Web
 {
     public class Module : ModuleBase, ISupportExportImportModule
     {
-        private const string _connectionStringName = "VirtoCommerce";
+        private static readonly string _connectionString = ConnectionStringHelper.GetConnectionString("VirtoCommerce");
         private readonly IUnityContainer _container;
 
         public Module(IUnityContainer container)
@@ -44,7 +44,7 @@ namespace VirtoCommerce.CoreModule.Web
 
         public override void SetupDatabase()
         {
-            using (var db = new CommerceRepositoryImpl(_connectionStringName, _container.Resolve<AuditableInterceptor>()))
+            using (var db = new CommerceRepositoryImpl(_connectionString, _container.Resolve<AuditableInterceptor>()))
             {
                 var initializer = new SetupDatabaseInitializer<CommerceRepositoryImpl, Data.Migrations.Configuration>();
                 initializer.InitializeDatabase(db);
@@ -61,7 +61,7 @@ namespace VirtoCommerce.CoreModule.Web
 
             #region Commerce
 
-            _container.RegisterType<IСommerceRepository>(new InjectionFactory(c => new CommerceRepositoryImpl(_connectionStringName, new EntityPrimaryKeyGeneratorInterceptor(), _container.Resolve<AuditableInterceptor>())));
+            _container.RegisterType<IСommerceRepository>(new InjectionFactory(c => new CommerceRepositoryImpl(_connectionString, new EntityPrimaryKeyGeneratorInterceptor(), _container.Resolve<AuditableInterceptor>())));
             _container.RegisterType<ICommerceService, CommerceServiceImpl>();
 
             #endregion
@@ -89,23 +89,17 @@ namespace VirtoCommerce.CoreModule.Web
 
             _container.RegisterType<IIndexingManager, IndexingManager>();
 
-            string connectionString = null;
+            var searchConnectionString = ConnectionStringHelper.GetConnectionString("SearchConnectionString");
 
-            var configConnectionString = ConfigurationManager.ConnectionStrings["SearchConnectionString"];
-            if (configConnectionString != null)
-            {
-                connectionString = configConnectionString.ConnectionString;
-            }
-
-            if (string.IsNullOrEmpty(connectionString))
+            if (string.IsNullOrEmpty(searchConnectionString))
             {
                 var settingsManager = _container.Resolve<ISettingsManager>();
-                connectionString = settingsManager.GetValue("VirtoCommerce.Search.SearchConnectionString", string.Empty);
+                searchConnectionString = settingsManager.GetValue("VirtoCommerce.Search.SearchConnectionString", string.Empty);
             }
 
-            if (!string.IsNullOrEmpty(connectionString))
+            if (!string.IsNullOrEmpty(searchConnectionString))
             {
-                var searchConnection = new SearchConnection(connectionString);
+                var searchConnection = new SearchConnection(searchConnectionString);
                 _container.RegisterInstance<ISearchConnection>(searchConnection);
             }
 
