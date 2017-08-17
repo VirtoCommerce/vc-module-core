bc4594dd4ff4a4a33973d2c3396ce19bda394a24
Andrew Orlov <ao@virtoway.com>
2017-08-17 16:37:59 +0200

    [publich] v2.24.8

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 // <copyright file="GlobalAssemblyInfo.cs" company="VirtoCommerce">
 //   Copyright © VirtoCommerce. All rights reserved.
 // </copyright>
@@ -15,8 +15,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Virto Commerce Core Module")]
 [assembly: AssemblyCopyright("Copyright © VirtoCommerce 2011-2017")]
 
-[assembly: AssemblyFileVersion("2.24.7.0")]
-[assembly: AssemblyVersion("2.24.7.0")]
+[assembly: AssemblyFileVersion("2.24.8.0")]
+[assembly: AssemblyVersion("2.24.8.0")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
