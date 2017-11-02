27b7e7a54b6aaef6054f6a8ca98d5270bc7f335c
artem-dudarev <ad@virtoway.com>
2017-11-02 11:54:30 +0200

    Added ability to override dynamic expression tree template

--- VirtoCommerce.CoreModule.Web/Scripts/common/dynamicExpression/expression-directives.js
+++ VirtoCommerce.CoreModule.Web/Scripts/common/dynamicExpression/expression-directives.js
@@ -1,39 +1,46 @@
-﻿angular.module('virtoCommerce.coreModule.common')
-.factory('virtoCommerce.coreModule.common.dynamicExpressionService', function () {
-    var retVal = {
-        expressions: [],
-        registerExpression: function (expression) {
-            if (!expression.templateURL) {
-                expression.templateURL = 'expression-' + expression.id + '.html';
-            }
-
-            this.expressions[expression.id] = expression;
-        }
-    };
-    return retVal;
-})
-.directive('vaDynamicExpressionTree', function () {
-    return {
-        restrict: 'E',
-        //scope: {
-        //    source: '='
-        //},
-        link: function ($scope, $element, $attrs) {
-            $scope.addChild = function (chosenMenuElement, parentBlock) {
-                if (!parentBlock.children) {
-                    parentBlock.children = [];
+angular.module('virtoCommerce.coreModule.common')
+    .factory('virtoCommerce.coreModule.common.dynamicExpressionService', function () {
+        var retVal = {
+            expressionTreeTemplateUrl: '',
+            expressions: [],
+            registerExpression: function (expression) {
+                if (!expression.templateURL) {
+                    expression.templateURL = 'expression-' + expression.id + '.html';
                 }
-                parentBlock.children.push(angular.copy(chosenMenuElement));
-            };
-            $scope.deleteChild = function (child, parentList) {
-                parentList.splice(parentList.indexOf(child), 1);
-            }
 
-            $scope.$watch($attrs.source, function (newVal) {
-                $scope.source = newVal;
-            });
-        },
-        templateUrl: 'Modules/$(VirtoCommerce.Core)/Scripts/common/dynamicExpression/expression-tree.tpl.html'
-    };
-})
-;
+                this.expressions[expression.id] = expression;
+            }
+        };
+        return retVal;
+    })
+    .directive('vaDynamicExpressionTree', function () {
+        return {
+            restrict: 'E',
+            //scope: {
+            //    source: '='
+            //},
+            link: function ($scope, $element, $attrs) {
+                $scope.addChild = function (chosenMenuElement, parentBlock) {
+                    if (!parentBlock.children) {
+                        parentBlock.children = [];
+                    }
+                    parentBlock.children.push(angular.copy(chosenMenuElement));
+                };
+                $scope.deleteChild = function (child, parentList) {
+                    parentList.splice(parentList.indexOf(child), 1);
+                }
+
+                $scope.$watch($attrs.source, function (newVal) {
+                    $scope.source = newVal;
+                });
+
+                $scope.templateUrl = 'Modules/$(VirtoCommerce.Core)/Scripts/common/dynamicExpression/expression-tree.tpl.html';
+                $scope.$watch($attrs.templateUrl, function (value) {
+                    if (value) {
+                        $scope.templateUrl = value;
+                    }
+                });
+            },
+            template: '<div ng-include="templateUrl"></div>'
+        };
+    });
