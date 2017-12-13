fc22517f219cd21c5b1b5423aacbb4678bed5914
Tatarincev Eugeney <et@virtoway.com>
2017-12-13 15:41:00 +0200

    Added some comment

--- VirtoCommerce.Domain/Pricing/Model/PriceEvaluationContext.cs
+++ VirtoCommerce.Domain/Pricing/Model/PriceEvaluationContext.cs
@@ -13,7 +13,10 @@ namespace VirtoCommerce.Domain.Pricing.Model
 		public string StoreId { get; set; } 
 		public string CatalogId { get; set; }
 		public string[] ProductIds { get; set; }
+        // The ordered list of price-list IDs, evaluation logic will return only matched product prices from the first price-list from this list
+        // To return all the prices found, simply set ReturnAllMatchedPrices to true
 		public string[] PricelistIds { get; set; }
+        //Set this flag to true for return all matched prices from all given pricelists 
 	    public bool ReturnAllMatchedPrices { get; set; }
         public decimal Quantity { get; set; }
 		public string CustomerId { get; set; }
