57cb03b87eefaa1270b7bf7ab9eaae1b051cb3d0
Els Van Zegbroeck <els.vanzegbroeck@delawareconsulting.com>
2017-12-13 14:41:25 +0100

    Add GetPricePerCurrencyByPriority to price evaluation context  (#24)
    * Add GetPricePerCurrencyByPriority to price evaluation context (needed in new price module update).
    
    * Rename flag to ReturnAllMatchedPrices, which defaults to false.
    
    * Added some comment

--- VirtoCommerce.Domain/Pricing/Model/PriceEvaluationContext.cs
+++ VirtoCommerce.Domain/Pricing/Model/PriceEvaluationContext.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,8 +13,12 @@ namespace VirtoCommerce.Domain.Pricing.Model
 		public string StoreId { get; set; } 
 		public string CatalogId { get; set; }
 		public string[] ProductIds { get; set; }
+        // The ordered list of price-list IDs, evaluation logic will return only matched product prices from the first price-list from this list
+        // To return all the prices found, simply set ReturnAllMatchedPrices to true
 		public string[] PricelistIds { get; set; }
-		public decimal Quantity { get; set; }
+        //Set this flag to true for return all matched prices from all given pricelists 
+	    public bool ReturnAllMatchedPrices { get; set; }
+        public decimal Quantity { get; set; }
 		public string CustomerId { get; set; }
 		public string OrganizationId { get; set; }
 		public DateTime? CertainDate { get; set; }
