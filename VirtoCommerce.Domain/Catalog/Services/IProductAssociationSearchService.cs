da25694be2c02550f4187a6fab34b04e199f023c
Yuliya Malik <yuliya.umbrellagroup@gmail.com>
2018-02-06 14:50:31 +0200

    type returned by interface was changed

--- VirtoCommerce.Domain/Catalog/Services/IProductAssociationSearchService.cs
+++ VirtoCommerce.Domain/Catalog/Services/IProductAssociationSearchService.cs
@@ -1,10 +1,11 @@
 using VirtoCommerce.Domain.Catalog.Model;
 using VirtoCommerce.Domain.Catalog.Model.Search;
+using VirtoCommerce.Domain.Commerce.Model.Search;
 
 namespace VirtoCommerce.Domain.Catalog.Services
 {
     public interface IProductAssociationSearchService
     {
-        SearchResult SearchProductAssociations(ProductAssociationSearchCriteria criteria);
+        GenericSearchResult<CatalogProduct> SearchProductAssociations(ProductAssociationSearchCriteria criteria);
     }
 }
