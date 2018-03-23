31f694f3c5e773be2732e49098852d392ca854e0
Nikolai Prohorov <prohorov.nn@yandex.ru>
2018-03-23 10:53:46 +0200

    Registration by invite (#52)
    * added generate invite link
    
    * fix notification recipient
    
    * Added  api method for registering by invite
    
    * Added description to api method
    
    * Fix register by invite notification template
    
    * Fix register by invite notification template
    
    * Added a message to  RegisterByInvite notification
    
    * rebranding names

--- VirtoCommerce.CoreModule.Web/Resources/NotificationResource.Designer.cs
+++ VirtoCommerce.CoreModule.Web/Resources/NotificationResource.Designer.cs
@@ -79,6 +79,24 @@ namespace VirtoCommerce.CoreModule.Web.Resources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Please  to register by invite clicking &lt;a href=&quot;{{ invite_url }}&quot;&gt;here&lt;/a&gt;.
+        /// </summary>
+        internal static string RegistrationInvitationNotificationBody {
+            get {
+                return ResourceManager.GetString("RegistrationInvitationNotificationBody", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Registration by invite.
+        /// </summary>
+        internal static string RegistrationInvitationNotificationSubject {
+            get {
+                return ResourceManager.GetString("RegistrationInvitationNotificationSubject", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Your user name - {{ user_name }}..
         /// </summary>
         internal static string RemindUserNameNotificationBody {
