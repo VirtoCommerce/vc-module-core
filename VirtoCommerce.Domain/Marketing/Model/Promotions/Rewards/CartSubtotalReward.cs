f102a276b7dc05bde42ea44b8e4d115c2ad4065a
Eugeny Tatarincev <et@virtoway.com>
2018-06-28 14:00:41 +0200

    Allow to be the CartSubtotalReward  relative and absolute

--- VirtoCommerce.Domain/Marketing/Model/Promotions/Rewards/CartSubtotalReward.cs
+++ VirtoCommerce.Domain/Marketing/Model/Promotions/Rewards/CartSubtotalReward.cs
@@ -6,22 +6,21 @@ using System.Runtime.Serialization;
 
 namespace VirtoCommerce.Domain.Marketing.Model
 {
-	public class CartSubtotalReward : PromotionReward
-	{
-		public CartSubtotalReward()
-		{
-		}
-		//Copy constructor
-		protected CartSubtotalReward(CartSubtotalReward other)
-			:base(other)
-		{
-			Amount = other.Amount;
-		}
-		public decimal Amount { get; set; }
+    public class CartSubtotalReward : AmountBasedReward
+    {
+        public CartSubtotalReward()
+        {
+        }
+        //Copy constructor
+        protected CartSubtotalReward(CartSubtotalReward other)
+            : base(other)
+        {
+            Amount = other.Amount;
+        }
 
-		public override PromotionReward Clone()
-		{
-			return new CartSubtotalReward(this);
-		}
-	}
+        public override PromotionReward Clone()
+        {
+            return new CartSubtotalReward(this);
+        }
+    }
 }
