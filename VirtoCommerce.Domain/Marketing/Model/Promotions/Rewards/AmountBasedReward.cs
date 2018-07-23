38893b6a65850a1b38704f2b591dede733d1910c
alivemen <alivemenone@gmail.com>
2018-07-23 13:44:32 +0300

    reformat

--- VirtoCommerce.Domain/Marketing/Model/Promotions/Rewards/AmountBasedReward.cs
+++ VirtoCommerce.Domain/Marketing/Model/Promotions/Rewards/AmountBasedReward.cs
@@ -7,25 +7,24 @@ using VirtoCommerce.Domain.Common;
 
 namespace VirtoCommerce.Domain.Marketing.Model
 {
-	public abstract class AmountBasedReward : PromotionReward
-	{
-		public AmountBasedReward()
-		{
-		}
+    public abstract class AmountBasedReward : PromotionReward
+    {
+        public AmountBasedReward()
+        {
+        }
 
-		protected AmountBasedReward (AmountBasedReward other)
-			:base(other)
-		{
-			AmountType = other.AmountType;
-			Amount = other.Amount;
-			Quantity = other.Quantity;
-		}
+        protected AmountBasedReward(AmountBasedReward other)
+            : base(other)
+        {
+            AmountType = other.AmountType;
+            Amount = other.Amount;
+            Quantity = other.Quantity;
+        }
 
-		public RewardAmountType AmountType { get; set; }
+        public RewardAmountType AmountType { get; set; }
 
-		public decimal Amount { get; set; }
-	    public decimal MaxLimit { get; set; }
-
+        public decimal Amount { get; set; }
+        public decimal MaxLimit { get; set; }
         public int Quantity { get; set; }
 
         public virtual decimal GetRewardAmount(decimal price, int quantity)
@@ -48,18 +47,18 @@ namespace VirtoCommerce.Domain.Marketing.Model
         }
 
         public decimal CalculateDiscountAmount(decimal price, int quantity = 1)
-		{
-			var retVal = Amount;
-			if (AmountType == RewardAmountType.Relative)
-			{
-				retVal = Math.Floor(price * Amount) * Math.Min(quantity, Quantity == 0 ? quantity : Quantity);
-			}
-			return FinanceRound(retVal);
-		}
+        {
+            var retVal = Amount;
+            if (AmountType == RewardAmountType.Relative)
+            {
+                retVal = Math.Floor(price * Amount) * Math.Min(quantity, Quantity == 0 ? quantity : Quantity);
+            }
+            return FinanceRound(retVal);
+        }
 
-		private static decimal FinanceRound(decimal value)
-		{
-			return Math.Round(value, 2, MidpointRounding.AwayFromZero);
-		}
-	}
+        private static decimal FinanceRound(decimal value)
+        {
+            return Math.Round(value, 2, MidpointRounding.AwayFromZero);
+        }
+    }
 }
