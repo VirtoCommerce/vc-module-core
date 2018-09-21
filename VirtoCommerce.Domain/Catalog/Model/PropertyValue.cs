c815e3e1d022ccfb7ef9ebc0b33a4ab96a4817cf
Eugeny Tatarincev <et@virtoway.com>
2018-09-21 11:04:36 +0200

    Removed obsolete code

--- VirtoCommerce.Domain/Catalog/Model/PropertyValue.cs
+++ VirtoCommerce.Domain/Catalog/Model/PropertyValue.cs
@@ -1,7 +1,4 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using VirtoCommerce.Domain.Commerce.Model;
 using VirtoCommerce.Platform.Core.Common;
 namespace VirtoCommerce.Domain.Catalog.Model
@@ -13,30 +10,7 @@ namespace VirtoCommerce.Domain.Catalog.Model
         public Property Property { get; set; }
         public string Alias { get; set; }
         public string ValueId { get; set; }
-
-        private object _value;
-        public object Value
-        {
-            get
-            {
-                var retVal = _value;
-                // Return actual dictionary property value from property meta-information instead stored
-                if (Property != null && Property.Dictionary && Property.DictionaryValues != null && ValueId != null)
-                {
-                    var dictValue = Property.DictionaryValues.FirstOrDefault(x => x.Id == ValueId);
-                    if (dictValue != null)
-                    {
-                        retVal = dictValue.Value;
-                    }
-
-                }
-                return retVal;
-            }
-            set
-            {
-                _value = value;
-            }
-        }
+        public object Value { get; set; }
 
         public PropertyValueType ValueType { get; set; }
         public string LanguageCode { get; set; }
@@ -46,30 +20,6 @@ namespace VirtoCommerce.Domain.Catalog.Model
             return (PropertyName ?? "unknown") + ":" + (Value ?? "undefined");
         }
 
-
-        /// <summary>
-        /// Returns for current value all dictionary values in all defined languages 
-        /// </summary>
-        /// <returns></returns>
-        [Obsolete]
-        public IEnumerable<PropertyValue> TryGetAllLocalizedDictValues()
-        {
-            var retVal = new List<PropertyValue>();
-
-            if (Property != null && Property.Dictionary && Property.Multilanguage && Property.DictionaryValues != null)
-            {
-                foreach (var dictValue in Property.DictionaryValues.Where(x => x.Alias == Alias))
-                {
-                    var langDictPropValue = this.Clone() as PropertyValue;
-                    langDictPropValue.Id = null;
-                    langDictPropValue.LanguageCode = dictValue.LanguageCode;
-                    langDictPropValue.Value = dictValue.Value;
-                    langDictPropValue.ValueId = dictValue.Id;
-                    retVal.Add(langDictPropValue);
-                }
-            }
-            return retVal;
-        }
 
         #region IInheritable Members
         public bool IsInherited { get; set; }
