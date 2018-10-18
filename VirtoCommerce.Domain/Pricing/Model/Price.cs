a4ceccf89ea1f4306065a8074035e3950d8e4610
Wim Vergouwe <wim.vergouwe@delawareconsulting.com>
2018-10-18 07:09:39 +0200

    Added comments to new price properties

--- VirtoCommerce.Domain/Pricing/Model/Price.cs
+++ VirtoCommerce.Domain/Pricing/Model/Price.cs
@@ -18,7 +18,17 @@ namespace VirtoCommerce.Domain.Pricing.Model
 		public decimal? Sale { get; set; }
 		public decimal List { get; set; }
 		public int MinQuantity { get; set; }
+
+        /// <summary>
+        /// Optional start date for this price, so that we can prepare prices ahead of time.
+        /// If start date equals now, this price will be active.
+        /// </summary>
 	    public DateTime? StartDate { get; set; }
+
+        /// <summary>
+        /// Optional end date for this price, so that we can prepare prices ahead of time.
+        /// If end date equals now, this price will not be active.
+        /// </summary>
 	    public DateTime? EndDate { get; set; }
 
         public decimal EffectiveValue
