5cbb3af349378ae44623d28b7c3cbdcf97fe6d5c
Alex Bolelov <alexx1524@gmail.com>
2019-03-20 13:55:27 +0300

    Added item WithPrices in the CustomerOrderResponseGroup (#115)
    * Added item WithPrices in the CustomerOrderResponseGroup

--- VirtoCommerce.Domain/Order/Model/CustomerOrderResponseGroup.cs
+++ VirtoCommerce.Domain/Order/Model/CustomerOrderResponseGroup.cs
@@ -1,20 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
 namespace VirtoCommerce.Domain.Order.Model
 {
-	[Flags]
-	public enum CustomerOrderResponseGroup
-	{
-		Default = 0,
-		WithItems = 1,
-		WithInPayments = 2,
-		WithShipments = 4,
-		WithAddresses = 8,
-		WithDiscounts = 16,
-		Full = WithItems | WithInPayments | WithShipments | WithAddresses | WithDiscounts
-	}
+    [Flags]
+    public enum CustomerOrderResponseGroup
+    {
+        Default = 0,
+        WithItems = 1,
+        WithInPayments = 2,
+        WithShipments = 4,
+        WithAddresses = 8,
+        WithDiscounts = 16,
+        WithPrices = 32,
+        Full = WithItems | WithInPayments | WithShipments | WithAddresses | WithDiscounts | WithPrices
+    }
 }
