e1529228d7d50375714ac54ccb2e944b90545f4f
Eugeniy Gusev <egu@virtoway.com>
2019-07-31 12:36:21 +0500

    Added TaxDetails to TaxRate class

--- VirtoCommerce.Domain/Tax/Model/TaxRate.cs
+++ VirtoCommerce.Domain/Tax/Model/TaxRate.cs
@@ -1,10 +1,17 @@
+using System.Collections.Generic;
 using Newtonsoft.Json;
+using VirtoCommerce.Domain.Commerce.Model;
 using VirtoCommerce.Platform.Core.Common;
 
 namespace VirtoCommerce.Domain.Tax.Model
 {
     public class TaxRate : ValueObject
     {
+        public TaxRate()
+        {
+            TaxDetails = new List<TaxDetail>();
+        }
+
         public decimal Rate { get; set; }
         public string Currency { get; set; }
 
@@ -12,5 +19,6 @@ namespace VirtoCommerce.Domain.Tax.Model
         [JsonIgnore]
         public TaxProvider TaxProvider { get; set; }
         public string TaxProviderCode { get; set; }
+        public ICollection<TaxDetail> TaxDetails { get; set; }
     }
 }
