a6273ce256e350ae725c91f534f715cff41e98a7
Andrey Kostyrin <kostyrin2@gmail.com>
2019-10-23 10:12:49 +0200

    #vp673 sync latest changes from 2.x version (#133)

--- src/VirtoCommerce.CoreModule.Web/Scripts/virtoCommerce-core.js
+++ src/VirtoCommerce.CoreModule.Web/Scripts/virtoCommerce-core.js
@@ -7,6 +7,44 @@ if (AppDependencies != undefined) {
 angular.module(moduleName, [
     'virtoCommerce.coreModule.packageType',
     'virtoCommerce.coreModule.currency',
+    'virtoCommerce.coreModule.fulfillment',
+    'virtoCommerce.coreModule.searchIndex',
     'virtoCommerce.coreModule.seo',
     'virtoCommerce.coreModule.common'
-]);
+])
+.run(['platformWebApp.metaFormsService', function (metaFormsService) {
+
+    metaFormsService.registerMetaFields('currencyDetail',
+        [
+            {
+                name: 'code',
+                title: 'core.blades.currency-detail.labels.code',
+                templateUrl: 'currencyDetail-code.html'
+            },
+            {
+                name: 'name',
+                title: 'core.blades.currency-detail.labels.name',
+                templateUrl: 'currencyDetail-name.html'
+            },
+            {
+                name: 'isPrimary',
+                title: 'core.blades.currency-detail.labels.is-primary',
+                templateUrl: 'currencyDetail-isPrimary.html'
+            },
+            {
+                name: 'exchangeRate',
+                title: 'core.blades.currency-detail.labels.exchange-rate',
+                templateUrl: 'currencyDetail-exchangeRate.html'
+            },
+            {
+                name: 'symbol',
+                title: 'core.blades.currency-detail.labels.symbol',
+                templateUrl: 'currencyDetail-symbol.html'
+            },
+            {
+                name: 'customFormatting',
+                title: 'core.blades.currency-detail.labels.custom-formatting',
+                templateUrl: 'currencyDetail-customFormatting.html'
+            }
+        ]);
+}]);
