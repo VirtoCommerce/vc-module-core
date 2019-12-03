ad36aa3bd165602a8f740d72a5efbdcb430fc217
Aleksandr Andreev <alivemenone@gmail.com>
2019-12-03 10:36:30 +0300

    added InventoryResponseGroup

---
+++ VirtoCommerce.Domain/Inventory/Model/InventoryResponseGroup.cs
@@ -0,0 +1,9 @@
+namespace VirtoCommerce.Domain.Inventory.Model
+{
+    public enum InventoryResponseGroup
+    {
+        None = 0,
+        OnlyInventory = 1,
+        WithAllFulfillmentCenters = 2
+    }
+}
