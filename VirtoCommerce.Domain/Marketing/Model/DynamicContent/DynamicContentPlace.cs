9fc747c4c1d7ae59e65918afdda443ff3f4f7d86
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2019-12-13 10:47:18 +0200

    Added properties to models (#143)

--- VirtoCommerce.Domain/Marketing/Model/DynamicContent/DynamicContentPlace.cs
+++ VirtoCommerce.Domain/Marketing/Model/DynamicContent/DynamicContentPlace.cs
@@ -1,21 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using VirtoCommerce.Platform.Core.Common;
 
 namespace VirtoCommerce.Domain.Marketing.Model
 {
-	public class DynamicContentPlace : AuditableEntity, IsHasFolder
-	{
-		public string Name { get; set; }
-		public string Description { get; set; }
-		public string ImageUrl { get; set; }
+    public class DynamicContentPlace : AuditableEntity, IsHasFolder
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public string Outline => Folder?.Outline;
+        public string Path => Folder?.Path;
 
-		#region IHasFolder Members
-		public string FolderId { get; set; }
-		public DynamicContentFolder Folder { get; set; }
-		#endregion
-	}
+        #region IHasFolder Members
+        public string FolderId { get; set; }
+        public DynamicContentFolder Folder { get; set; }
+        #endregion
+    }
 }
