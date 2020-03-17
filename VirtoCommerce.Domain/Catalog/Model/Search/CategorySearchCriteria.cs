5e1638973e0f0b6a36b6810c520b47f7383b4325
Aleksandr Andreev <alivemenone@gmail.com>
2020-03-17 16:40:28 +0300

    Add 'WithHidden' field to category search criteria

--- VirtoCommerce.Domain/Catalog/Model/Search/CategorySearchCriteria.cs
+++ VirtoCommerce.Domain/Catalog/Model/Search/CategorySearchCriteria.cs
@@ -1,9 +1,15 @@
-﻿using VirtoCommerce.Domain.Search;
+using VirtoCommerce.Domain.Search;
 
 namespace VirtoCommerce.Domain.Catalog.Model.Search
 {
     public class CategorySearchCriteria : CatalogSearchCriteriaBase
     {
         public override string ObjectType { get; set; } = KnownDocumentTypes.Category;
+
+        /// <summary>
+        /// Specifies if we search for hidden products.
+        /// </summary>
+        public virtual bool WithHidden { get; set; }
+
     }
 }
