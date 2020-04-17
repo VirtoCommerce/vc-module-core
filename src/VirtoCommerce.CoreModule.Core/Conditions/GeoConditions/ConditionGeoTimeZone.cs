254eb746ec3aa3ea3f32b0a473504357d5d2a903
Andrei Kostyrin <kostyrin2@gmail.com>
2020-04-17 15:24:55 +0200

    #VP-2003 fix geo contidition

--- src/VirtoCommerce.CoreModule.Core/Conditions/GeoConditions/ConditionGeoTimeZone.cs
+++ src/VirtoCommerce.CoreModule.Core/Conditions/GeoConditions/ConditionGeoTimeZone.cs
@@ -12,8 +12,9 @@ namespace VirtoCommerce.CoreModule.Core.Conditions.GeoConditions
         public override bool IsSatisfiedBy(IEvaluationContext context)
         {
             var result = false;
-            if (context is EvaluationContextBase evaluationContext && int.TryParse(evaluationContext.GeoTimeZone, out var geoTimeZone) && !string.IsNullOrEmpty(Value))
+            if (context is EvaluationContextBase evaluationContext && !string.IsNullOrEmpty(evaluationContext.GeoTimeZone) && !string.IsNullOrEmpty(Value))
             {
+                var geoTimeZone = TZConvert.GetTimeZoneInfo(evaluationContext.GeoTimeZone).BaseUtcOffset.TotalMilliseconds;
                 var valueTimeZone = TZConvert.GetTimeZoneInfo(Value).BaseUtcOffset.TotalMilliseconds;
                 var secondValueTimeZone = !string.IsNullOrEmpty(SecondValue) ? TZConvert.GetTimeZoneInfo(SecondValue).BaseUtcOffset.TotalMilliseconds : 0;
 
