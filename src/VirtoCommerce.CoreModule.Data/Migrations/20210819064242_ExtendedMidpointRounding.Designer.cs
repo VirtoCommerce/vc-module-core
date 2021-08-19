5857c521b101b96efd3dba1866c4f7d88139cfc7
Alexander Morogov <asmorogov@gmail.com>
2021-08-19 11:58:07 +0200

    PT-1924: Increase the length of MidpointRounding to 18

--- src/VirtoCommerce.CoreModule.Data/Migrations/20210819064242_ExtendedMidpointRounding.Designer.cs
+++ src/VirtoCommerce.CoreModule.Data/Migrations/20210819064242_ExtendedMidpointRounding.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VirtoCommerce.CoreModule.Data.Repositories;
 
 namespace VirtoCommerce.CoreModule.Data.Migrations
 {
     [DbContext(typeof(CoreDbContext))]
-    partial class CoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210819064242_ExtendedMidpointRounding")]
+    partial class ExtendedMidpointRounding
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
