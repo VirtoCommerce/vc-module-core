68b336b4c11c968980ad817c77dd89b02e63f974
Evgeny Okhrimenko <eo@virtoway.com>
2023-02-14 08:53:00 +0200

    Add Undefined address type (#211)

--- src/VirtoCommerce.CoreModule.Core/Common/AddressType.cs
+++ src/VirtoCommerce.CoreModule.Core/Common/AddressType.cs
@@ -5,6 +5,7 @@ namespace VirtoCommerce.CoreModule.Core.Common
     [Flags]
     public enum AddressType
     {
+        Undefined = 0,
         Billing = 1,
         Shipping = 2,
         Pickup = 4,
