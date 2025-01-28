b4b650b31d269de23c9cf6c5a1333990e148b253
Oleg Zhuk <zhukoo@gmail.com>
2025-01-28 12:16:42 +0200

    VCST-2486: Add LocalizedName to OutlineItemyear
    feat: The `OutlineItem` class in the `VirtoCommerce.CoreModule.Core.Outlines` namespace has been updated to include a new property `LocalizedName`, which is a dictionary for storing localized names.

--- src/VirtoCommerce.CoreModule.Core/Outlines/OutlineItem.cs
+++ src/VirtoCommerce.CoreModule.Core/Outlines/OutlineItem.cs
@@ -32,6 +32,9 @@ namespace VirtoCommerce.CoreModule.Core.Outlines
         /// The name of current item
         /// </summary>
         public string Name { get; set; }
+
+        public IDictionary<string, string> LocalizedName { get; set; }
+
         /// <summary>
         /// True when this object is linked to the virtual parent.
         /// </summary>
@@ -46,6 +49,7 @@ namespace VirtoCommerce.CoreModule.Core.Outlines
         {
             var result = MemberwiseClone() as OutlineItem;
             result.SeoInfos = SeoInfos?.Select(x => x.Clone() as SeoInfo).ToList();
+            result.LocalizedName = LocalizedName?.ToDictionary(x => x.Key, x => x.Value);
             return result;
         }
     }
