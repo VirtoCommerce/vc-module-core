6680329a1b9d896c5ccc84d8bb16b6645a630f54
Basil Kotov <basilkot@gmail.com>
2026-04-08 11:43:46 +0100

    Mark SEO frontend as obsolete (moved to Seo module)

--- src/VirtoCommerce.CoreModule.Web/Scripts/SEO/resources/seoResource.js
+++ src/VirtoCommerce.CoreModule.Web/Scripts/SEO/resources/seoResource.js
@@ -1,4 +1,5 @@
-﻿angular.module('virtoCommerce.coreModule.seo')
+﻿// Obsolete-VC0014: Moved to VirtoCommerce.Seo module (virtoCommerce.seo.seoApi)
+angular.module('virtoCommerce.coreModule.seo')
 .factory('virtoCommerce.coreModule.seoApi', ['$resource', function ($resource) {
     return $resource('api/seoinfos/duplicates', null, {
         batchUpdate: { url: 'api/seoinfos/batchupdate', method: 'PUT' }
